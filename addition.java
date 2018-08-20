a47f730e5b0fa008b1dc67b3ce2a77f46a3eda9a
Sparash sharma <sparash.27kpr@gmail.com>
2018-08-20 04:13:45 +0000

    assignment3

---
+++ addition.java
@@ -0,0 +1,12 @@
+class addition
+{
+    public static void main(String[] args)
+    {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c;
+
+        c=a+b;
+        System.out.println(c);
+    }
+}
---
+++ prime.java
@@ -0,0 +1,24 @@
+class prime
+{
+    public static void main(String[] args)
+    {
+        int num = Integer.parseInt(args[0]);
+        flag=false;
+        for(int i=2;i<=num/2;++i)
+        {
+            if(num%i==0)
+            {
+                flag=true;
+                break;
+            }
+        }
+        if(flag!=true)
+        {
+            System.out.println("It is a prime number");
+        }
+        else
+        {
+            System.out.println("It is not a prime number");
+        }
+    }
+}
